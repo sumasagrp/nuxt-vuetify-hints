ba353099ae305c45ee4cf863a92212204e4da6a8
santiagoaloi <santiagoaloi@gmail.com>
2024-02-22 18:06:11 +0100

    changes

--- components/ThemeSwitch.vue
+++ components/ThemeSwitch.vue
@@ -23,6 +23,7 @@ onMounted(() => {
   <client-only>
     <VSelect
       v-model="colorMode.preference"
+      mandatory
       :items="['dark', 'light', 'system']"
       @update:model-value="setTheme"
     />
